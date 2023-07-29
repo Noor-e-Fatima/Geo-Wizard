9bb3edeac62457a83b19ae08e32def16339fc571
Noor-e-Fatima <nfatima.5.2003@gmail.com>
2023-07-29 21:41:25 +0500

    changed

---
+++ resp.js
@@ -0,0 +1,11 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+rightnav=document.querySelector('.rightnav')
+navList=document.querySelector('.nav-list')
+
+burger.addEventListener('click', ()=>{
+rightNav.classlist.toggle('v-class-resp');
+nav-list.classlist.toggle('v-class-resp');
+navbar.classlist.toggle('h-nav-resp');
+}
+)
